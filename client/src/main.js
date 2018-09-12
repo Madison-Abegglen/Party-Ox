1389bb6a2aba55c015b6c5043ff5c91b8aa6f0d9
Christopher Brown <chbphone55@gmail.com>
2018-09-11 20:09:04 -0600

    automatic global registration of base components

--- client/src/main.js
+++ client/src/main.js
@@ -14,6 +14,32 @@ Vue.use(Vuetify, {
   }
 })
 
+const requireComponent = require.context(
+  // The relative path of the components folder
+  './components',
+  // Whether or not to look in subfolders
+  false,
+  // The regular expression used to match base component filenames
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  // Get component config
+  const componentConfig = requireComponent(fileName)
+
+  // Get name of component (already in PascalCase)
+  const componentName = fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+
+  // Register component globally
+  Vue.component(
+    componentName,
+    // Look for the component options on `.default`, which will
+    // exist if the component was exported with `export default`,
+    // otherwise fall back to module's root.
+    componentConfig.default || componentConfig
+  )
+})
+
 Vue.config.productionTip = false
 
 new Vue({
