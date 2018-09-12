4a3d730a89ba43f91a3cd2ec459bd6e45fcaef16
Christopher Brown <chbphone55@gmail.com>
2018-09-11 23:13:52 -0600

    Header with title component

---
+++ client/src/components/TitleHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <base-header>
+    <h6 class='title' slot='before'>{{ $props.title }}</h6>
+  </base-header>
+</template>
+
+<script>
+  import BaseHeader from '@/components/BaseHeader'
+
+  export default {
+    extends: BaseHeader,
+    name: 'title-header',
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped lang='scss'>
+
+</style>
