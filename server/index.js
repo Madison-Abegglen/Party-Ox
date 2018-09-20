c14fa1104936c2b88e1980646c609fd62df79229
Christopher Brown <chbphone55@gmail.com>
2018-09-19 21:14:04 -0600

    serve on correct port

--- server/index.js
+++ server/index.js
@@ -161,6 +161,6 @@ io.on('connection', socket => {
 server.use('/api/*', (error, req, res) => res.status(400).send(error))
 server.use('*', (req, res) => res.status(404).send('<h1>404 NO PAGE HERE</h1>'))
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Sockets & Server started on port: ${port}`))
 // server.listen(port, () => console.log(`Server started on port: ${port}`))
