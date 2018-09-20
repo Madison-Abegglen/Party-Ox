9b5abaabef2bcc482bb95cc40f4870c4c17e6478
Christopher Brown <chbphone55@gmail.com>
2018-09-19 21:01:39 -0600

    push dist to heroku

---
+++ client/dist/precache-manifest.53e52d4365398c53ebebb9ed448c1065.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d0ca0f5a45e9788ccf13",
+    "url": "/js/chunk-vendors.5e91f36e.js"
+  },
+  {
+    "revision": "46dc8f7e575cd4dd41c9",
+    "url": "/js/app.8235ec1d.js"
+  },
+  {
+    "revision": "e34a9bce5d1ec4eea47ab90701343d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f7dba37dced0667a352570665369050",
+    "url": "/img/ox.9f7dba37.svg"
+  },
+  {
+    "revision": "9f7dba37dced0667a352570665369050",
+    "url": "/img/ox-party.9f7dba37.svg"
+  },
+  {
+    "revision": "4329c299a5a2112dc7d0652826d8bc24",
+    "url": "/img/ox-circle.4329c299.svg"
+  },
+  {
+    "revision": "d0ca0f5a45e9788ccf13",
+    "url": "/css/chunk-vendors.4309f68d.css"
+  },
+  {
+    "revision": "46dc8f7e575cd4dd41c9",
+    "url": "/css/app.b95e23ef.css"
+  }
+];
