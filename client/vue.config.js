789b5d45b3f661c1a7054e71a23b300455f88724
Christopher Brown <chbphone55@gmail.com>
2018-10-30 13:14:36 -0600

    precaching globPatterns

--- client/vue.config.js
+++ client/vue.config.js
@@ -5,7 +5,11 @@ module.exports = {
     themeColor: '#66fcf1',
     workboxOptions: {
       swSrc: 'public/sw.js',
-      swDest: 'sw.js'
+      swDest: 'sw.js',
+      globPatterns: [
+        /*eslint no-useless-escape: "off"*/
+        '**\/*.{js,css,html,png}',
+      ]
     }
   },
 
