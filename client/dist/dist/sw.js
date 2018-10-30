37009353ef69a6375c193cf17774982c8e3507ee
Christopher Brown <chbphone55@gmail.com>
2018-10-30 12:33:30 -0600

    eslint configuration for sw.js

--- client/dist/dist/sw.js
+++
@@ -1,3 +0,0 @@
-importScripts("/precache-manifest.45f7f6c339cfbfc6f64ecffb78e61dae.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
-
-
---
+++ client/public/sw.js
@@ -0,0 +1,10 @@
+/* eslint-env serviceworker */
+/* global workbox */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
